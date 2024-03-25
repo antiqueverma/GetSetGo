d46c680a585f01ba1734fc8a77588342209181ca
Manish Verma <antiqueverma@gmail.com>
2024-03-25 14:45:38 +0530

    Reorganized Files
    Moved files to their proper sub folders

--- GetSetGo/sys/core/sys_core.h
+++ GetSetGo/sys/core/sys_core.h
@@ -3,10 +3,25 @@
 #ifndef GSG_CORE_H_
 #define GSG_CORE_H_
 
-
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+/*Standard Constants*/
+#define PASS 	1
+#define FAIL	0
+#define TRUE 	1
+#define FALSE	0
+#define VALID	1
+#define INVALID	0
+#define ENABLE	1
+#define DISABLE	0
+
+#include "sys_config.h"
+#include "string.h"
 
 
+#include "queue.h"
 
 
 #endif
