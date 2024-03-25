3557421323e92b64eb25baf303d349918a25e122
Manish Verma <antiqueverma@gmail.com>
2024-03-25 14:43:17 +0530

    Queue Module Passed
    Queue module tested for FIFO and LIFO type Queues with multiple test cases

---
+++ GetSetGo/sys/core/sys_core.h
@@ -0,0 +1,28 @@
+
+
+#ifndef GSG_CORE_H_
+#define GSG_CORE_H_
+
+
+#include <stdint.h>
+
+
+
+
+#endif
+
+/*
+
+#ifndef GSG_XYZ_H_
+#define GSG_XYZ_H_
+
+
+#include <core.h>
+
+
+
+
+#endif
+
+
+ * */
---
+++ GetSetGo/sys/queue/queue.h
@@ -0,0 +1,12 @@
+
+
+#ifndef GSG_CORE_H_
+#define GSG_CORE_H_
+
+
+#include <core.h>
+
+
+
+
+#endif
